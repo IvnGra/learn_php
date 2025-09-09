801df81a550a109ac54e43d28358715bb7bae124
ivan gra <gratsov.ivan@gmail.com>
2025-09-09 10:50:37 +0300

    examples of loops and foreach

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-for($i=0; $<10;$i++){
+for($i=0; $i<10; $i++){
     var_dump($i);
 }
 
@@ -12,16 +12,17 @@ for($i=1;$i<1000000;$i*=2){
     var_dump($i);
 }
 
-for(;;){
+// Infinite loop removed for safety
 
-}
 
-$time = time()
-while(time()<$time+10){
+$time = time();
+$i = 0;
+while(time() < $time + 10){
     var_dump(time());
     $i++;
 }
 var_dump($i);
+
 
 $i = 10;
 while($i < 10){
@@ -29,14 +30,24 @@ while($i < 10){
 }
 do {
     var_dump('DO');
-} while($i <10);
+} while($i < 10);
+
 
 $array = [1,2,3];
 foreach($array as $value){
     var_dump($value);
-};
+}
+
 
 $a = 1;
-$b =$a;
+$b = $a;
 $b = 2;
 var_dump($a);
+
+
+$array = [1,2,3];
+foreach($array as &$value){
+    $value *= 2;
+}
+unset($value); // break reference
+var_dump($array);
