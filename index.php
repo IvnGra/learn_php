8809b7e9054b09cc7a3edfe0400a3f9d0c8c1802
ivan gra <gratsov.ivan@gmail.com>
2025-09-16 10:06:45 +0300

    add magic method example

--- index.php
+++ index.php
@@ -1,44 +1,51 @@
 <?php
 
 class Cat {
-    public function __construct() 
+    public function __construct()
     {
         var_dump('Class was created');
     }
-
-    public function __call($name, $value) {
-        var_dump($name, $value);
+    public function __call($name, $args) {
+        var_dump($name, $args);
     }
 
-    public function __get() {
+    public function __invoke($value)
+    {
+        var_dump($value);
+    }
+
+    public function __get($name){
         var_dump($name);
         return 'Cool shiiit';
     }
 
-    public function __set($name, $value) {
+    public function __set($name, $value){
         var_dump($name, $value);
+       
     }
 
     public function __toString()
     {
-        return 'mjäu';
+        return 'Mjäu';
     }
 
     public function __destruct()
     {
-        var_dump('class was destroyed');
+        var_dump('Class was destroyed');
     }
 }
 
-function makeCat() {
+function makeCat(){
     $cat = new Cat();
 }
 
+makeCat();
 $kitty = new Cat();
 var_dump($kitty);
 var_dump($kitty->mood);
-$kitty->color 
-$kitty->thirowShit('')
-echo($kitty);
+$kitty->color = 'Rainbow';
+$kitty->throwShit('many', 'smelly', 'poops', 1 , 2 , 3);
+$kitty('something');
+echo $kitty;
 $kitty = 1;
 var_dump('something');
